9f9b187036fe126dc898af72cb8dd7bdc1af2406
LazyOIGG <nichunyang2005@foxmail.com>
2025-12-18 11:58:27 +0800

    Mapper层 完成自定义复杂方法的映射文件

--- src/main/java/ynu/edu/dao/IOrderDetailetDao.java
+++ src/main/java/ynu/edu/dao/IOrderDetailetDao.java
@@ -2,8 +2,16 @@ package ynu.edu.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import ynu.edu.entity.OrderDetailet;
+
+import java.util.List;
 
 @Mapper
 public interface IOrderDetailetDao extends BaseMapper<OrderDetailet> {
+    // 批量保存订单明细
+    int saveOrderDetailetBatch(@Param("list") List<OrderDetailet> list);
+
+    // 根据订单ID查询订单明细
+    List<OrderDetailet> listOrderDetailetByOrderId(@Param("orderId") Integer orderId);
 }
